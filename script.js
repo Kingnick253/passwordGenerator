c63e1840bd231fe9d25ff40ae0602d8316a724e8
Kingnick253 <nicholaswebb2016@gmail.com>
2022-06-17 10:07:38 -0700

    added pseudocode

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 //
 function generatePassword(){
-  console.log("button workS")
+  console.log("button work")
+  // 1.prompt the user for password criteria
+  var userprompt = prompt("How long do you want your password");
+  // a. password length between 8-128.
+  // b. prompt user if they want lowercase uppercase special char
+  // 2. validate the input
+  // 3. display the generated password on the page.
+  
+  return "Generated password"
 }
 // Write password to the #password input
 function writePassword() {
