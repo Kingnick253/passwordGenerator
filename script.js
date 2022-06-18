ad623868945bf93fef3d59f930dc8bbbbedf3e84
Kingnick253 <nicholaswebb2016@gmail.com>
2022-06-18 08:46:04 -0700

    Started password criteria

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
-//
+var passLength;// input for password length
+var passCase;// input for lowercase or uppercase
+var passLCase;// uses user input if password is lowercase
+var passUCase;// uses user input if password is uppercase
+var passSpecial;// do you want special char or not 
+
 function generatePassword(){
   console.log("button work")
   // 1.prompt the user for password criteria
-  var userprompt = prompt("How long do you want your password");
+  passLength = prompt("Enter paspassword length 8-128");
+
+  if(passLength >= 8 && passLength <= 128){
+    passCase = prompt("Do you want the to be lowercase or uppercase");
+    
+  }  else if (passCase === "lowercase") {
+    passSpecial = prompt("Do you want speical Character");
+    debugger;
+  
+  } else if(passCase === "uppercase"){
+    passSpecial = prompt("Do you want speical Character");
+  }else{
+    alert("invaild input");
+  }
+
   // a. password length between 8-128.
   // b. prompt user if they want lowercase uppercase special char
   // 2. validate the input
-  // 3. display the generated password on the page.
+  // 3. generate password based on criteria 
+
+
+  // 4. display the generated password on the page.
   
   return "Generated password"
 }
